479de5cd85d90e7e87ca7d737e5cf57b59e80aa9
Sebastian 3 <githubseb.contact@gmail.com>
2017-05-01 12:54:05 +0200

    added datatype aliases
    + XMath_Int32
    + XMath_Uint32
    + XMath_Int64
    + XMath_Uint64
    + XMath_Float
    + XMath_Double
    + XMath_LongDouble

--- include/XMath_Types.hpp
+++ include/XMath_Types.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+#ifndef XMATH_TYPES_HPP
+#define XMATH_TYPES_HPP
+
+namespace XMathematics
+{
+	typedef int XMath_Int32;						/* normal (signed) 32-bit integer */
+	typedef unsigned int XMath_Uint32;				/* unsigned 32-bit integer */
+	typedef long long XMath_Int64;					/* (signed) 64-bit integer */
+	typedef unsigned long long XMath_Uint64;		/* unsigned 64-bit integer */
+	typedef float XMath_Float;						/* floating point number */
+	typedef double XMath_Double;					/* floating point with double precision */
+	typedef long double XMath_LongDouble;			/* long double datatype */
+};
+
+#endif
